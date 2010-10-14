9bc8f7411db22d4bc8226412ad919c21b5c3cfbe
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-10-14 21:40:42 +0200

    Initialize devise, drop some unused files.

---
+++ cyt_app.rb
@@ -0,0 +1,12 @@
+run "rm README"
+
+run "rm public/index.html"
+
+run "cp templates/Gemfile"
+run "cp templates/.gitignore"
+
+generate "devise:install"
+generate "devise", "User"
+
+rake "db:migrate"
+cp "templates/simple_navigation.rb" "config/initializers/simple_navigation.rb"
