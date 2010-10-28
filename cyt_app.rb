efed4795a74e4ff960190c63ab1af4aba1aa8860
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-10-28 08:43:24 +0200

    Use copy_file, not cp in cyt_app.rb

--- cyt_app.rb
+++ cyt_app.rb
@@ -3,17 +3,17 @@ run "rm README"
 
 run "rm public/index.html"
 
-run "cp templates/Gemfile"
+copy_file "templates/Gemfile", "Gemfile"
 run "cp templates/.gitignore"
 
 # Authorization
 generate "devise:install"
 generate "devise", "User"
-cp "templates/app/views/devise" "app/views/devise"
+copy_file "templates/app/views/devise", "app/views/devise"
 rake "db:migrate"
 
 # Navigation
-cp "templates/config/initializers/simple_navigation.rb" "config/initializers/simple_navigation.rb"
+copy_file "templates/config/initializers/simple_navigation.rb", "config/initializers/simple_navigation.rb"
 
 # Styling
 generate "styleyt:theme"
