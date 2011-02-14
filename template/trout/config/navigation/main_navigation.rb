c0a875c6438d10005468ccaa4863689a75b9d43b
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-02-14 08:11:47 +0100

    Add main_navigation.rb

---
+++ template/trout/config/navigation/main_navigation.rb
@@ -0,0 +1,16 @@
+# Configures your navigation
+SimpleNavigation::Configuration.run do |navigation|  
+  # Define the primary navigation
+  navigation.items do |primary|
+    # Application navigation
+    if current_user
+      primary.item :overview, t('main_navigation.overview'), home_welcome_path
+      
+#      primary.item :nav_users, t_model(User), users_path, :if => lambda { can?(:index, User) } do |user|
+#        user.item :new_user, t_action(:new, User), new_user_path, :if => lambda { can?(:new, User) }
+#       user.item :user_index, t_action(:index, User), users_path, :if => lambda { can?(:index, User) }
+#       user.item :current_user, t_title(:current, User), current_users_path
+      end
+    end
+  end
+end
