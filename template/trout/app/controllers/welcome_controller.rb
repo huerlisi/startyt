2cf6381448825140e1359d0cef80dc17fc20fb10
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-02-14 08:03:30 +0100

    Add files for landing page.

---
+++ template/trout/app/controllers/welcome_controller.rb
@@ -0,0 +1,14 @@
+# WelcomeController dispatches from landing page.
+#
+# It dispatches the landing page according to the users role or other conditions.
+class WelcomeController < ApplicationController
+  def index
+    # Login page if user not signed in
+    unless user_signed_in?
+      redirect_to welcome_home_path 
+      return
+    end
+
+    # Here comes app specific dispatch
+  end
+end
