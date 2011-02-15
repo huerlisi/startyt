aeb343179bb01fed04bdd164e9bf41e96f91d5c9
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-02-15 07:44:48 +0100

    Use Rails.root, not RAILS_ROOT in simple_navigation initializer.

--- template/trout/config/initializers/simple_navigation.rb
+++ template/trout/config/initializers/simple_navigation.rb
@@ -1,1 +1,1 @@
-SimpleNavigation.config_file_path = default_config_file_path = File.join(RAILS_ROOT, 'config', 'navigation')
+SimpleNavigation.config_file_path = default_config_file_path = Rails.root.join('config', 'navigation')
